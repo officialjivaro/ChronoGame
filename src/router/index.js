4fea3ca769acc10b35a97a9a9a6d3940ce9f505e
officialjivaro <noreply.jivaro@gmail.com>
2025-03-18 23:06:41 +0900

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GameView from '../views/GameView.vue'
 import ResultsView from '../views/ResultsView.vue'
+
 const routes = [
-  { path: '/', name: 'Home', component: HomeView },
-  { path: '/game', name: 'Game', component: GameView },
+  { path: '/',      name: 'Home',    component: HomeView },
+  { path: '/game',  name: 'Game',    component: GameView },
   { path: '/results', name: 'Results', component: ResultsView }
 ]
+
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
